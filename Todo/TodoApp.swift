962b0d57052cb77f3221c2f5532ab49ce778a1ac
Jesus Antonio Cruz Suarez <jesus.cruz@alternova.com>
2024-04-18 18:48:43 -0500

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Jesus Cruz Suárez on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
